f380c6f4b2755aee4601f565a4a93305bc6460ea
malek1995 <38815904+malek1995@users.noreply.github.com>
2020-07-27 17:03:55 +0200

    app start

---
+++ Game/Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Codecamp on 27.07.20.
+//  Copyright © 2020 Codecamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
